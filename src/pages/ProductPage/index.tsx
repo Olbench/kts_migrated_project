be07185822052aae8f570ea0f57b9db0b08fc2b8
Olbench <olbena.m@gmail.com>
2026-03-19 13:35:36 +0400

    Make pages Products/ProductPage placeholders

---
+++ src/pages/ProductPage/index.tsx
@@ -0,0 +1,5 @@
+import PlaceholderPage from '@/screens/PlaceholderPage/PlaceholderPage'
+
+export default function ProductPageRoute() {
+  return <PlaceholderPage title="Product" />
+}
---
+++ src/pages/Products/index.tsx
@@ -0,0 +1,5 @@
+import PlaceholderPage from '@/screens/PlaceholderPage/PlaceholderPage'
+
+export default function ProductsRoute() {
+  return <PlaceholderPage title="Products" />
+}
