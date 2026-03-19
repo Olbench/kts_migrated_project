6be2fce2a382de68e73b1a5f37cda285e44556b3
Olbench <olbena.m@gmail.com>
2026-03-19 06:18:10 +0400

    Wrap CartPage with StoreProvider

--- src/pages/CartPage/index.ts
+++ src/pages/CartPage/index.ts
@@ -1,1 +1,10 @@
-export { default } from '@/screens/CartPage/CartPage'
+import StoreProvider from '@/providers/StoreProvider'
+import CartPage from '@/screens/CartPage/CartPage'
+
+export default function CartPageRoute() {
+  return (
+    <StoreProvider>
+      <CartPage />
+    </StoreProvider>
+  )
+}
